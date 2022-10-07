b06fdfc21dd5c5df86cbaa23e438e9404056da4f
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-10-07 15:19:34 +0100

    Added a lot of stuff :)

---
+++ Chocolate/ChocolateBar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Chocolate
+{
+    class ChocolateBar
+    {
+
+        public ChocolateBar(string Name, double WeightInG = 0, bool Melted = false, int Calories = 0)
+        {
+            this.Name = Name;
+            this.WeightInG = WeightInG;
+            this.Melted = Melted;
+            this.Calories = Calories;
+        }
+
+        /// <summary>
+        /// Number of calories per chocolate bar
+        /// </summary>
+        public int Calories;
+
+        /// <summary>
+        /// Weight in grams per chocolate bar
+        /// </summary>
+        public double WeightInG;
+
+        /// <summary>
+        /// Name of chocolate bar
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Whether it's melted or not
+        /// </summary>
+        public bool Melted;
+
+        /// <summary>
+        /// Enjoy the chocolate bar goodness
+        /// </summary>
+        public void Eat()
+        {
+            Console.WriteLine($"Yum! You just ate a {Name} which was {Calories} calories and weighed {WeightInG}g");
+            WeightInG = 0;
+        }
+
+        /// <summary>
+        /// Throw the chocolate bar
+        /// </summary>
+        public void Throw()
+        {
+            Console.WriteLine("Ow!");
+        }
+    }
+}
--- Chocolate/Program.cs
+++ Chocolate/Program.cs
@@ -10,7 +10,11 @@ namespace Chocolate
     {
         static void Main(string[] args)
         {
+            ChocolateBar boost = new ChocolateBar("Boost", 31.5, false, 163);
+            ChocolateBar rolo = new ChocolateBar("Rolo", 41.6, false, 200);
 
+            boost.Eat();
+            rolo.Eat();
 
         }
     }
